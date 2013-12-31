338b7c1f8f89a3e46c4b812d4b03fc0800c32b96
Massyl Nait-Mouloud <massil.nait@gmail.com>
2013-12-31 15:42:43 +0100

    some config scripts added

---
+++ batteryManager.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+battery_charging=`acpi -b | awk '{print $3}'|awk -F"," '{print $1}'`
+battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
+
+export DISPLAY=:0
+
+if [ $battery_level -le 20 ] && [ $battery_charging = "Discharging" ]
+then
+
+    notify-send "Battery low : ${battery_level}%!"
+
+fi
