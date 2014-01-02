9f414299cbd6a3ddf8757ade1356f0d2427f669c
Massyl Nait-Mouloud <massil.nait@gmail.com>
2014-01-02 14:28:52 +0100

    script enhaced to notify once at 20% and every minute <= 10%

--- batteryManager.sh
+++ batteryManager.sh
@@ -5,5 +5,4 @@ battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
 
 export DISPLAY=:0
 
-[ $battery_level -le 20 ] && [ $battery_charging = "Discharging" ] && notify-send "Battery low : ${battery_level}%!"
-
+[ $battery_level -eq 20  -o $battery_level -le 10 ] && [ $battery_charging = "Discharging" ] && notify-send "Battery low : ${battery_level}%!"
