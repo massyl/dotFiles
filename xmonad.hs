3aee198ad3923f5cdebbd1562636518834ea7884
massyl <ext.mnaitmouloud@kyriba.com>
2014-08-11 10:03:38 +0200

    work office config

--- xmonad.hs
+++ xmonad.hs
@@ -23,6 +23,11 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig(additionalKeys)
 import System.Exit
 import XMonad.Actions.WindowGo
+import qualified XMonad.StackSet as W
+import XMonad.Hooks.SetWMName
+import qualified XMonad.Layout.IndependentScreens as LIS
+import XMonad.Actions.UpdatePointer
+import XMonad.Layout.Spacing
 
 customWs = ["1:func","2:java","3:web","4:vms"] ++ map show [5..9]
 
@@ -37,7 +42,7 @@ mateConfig = desktopConfig
     , keys     = mateKeys <+> keys desktopConfig
     , workspaces = customWs
     , manageHook = manageDocks <+> ownManageHook
-   -- , startUpHook = stHook
+    , startupHook = setWMName "LG3D"
     }
 
 mateKeys (XConfig {modMask = modm}) = M.fromList $
@@ -59,28 +64,43 @@ mateKeys (XConfig {modMask = modm}) = M.fromList $
    -- disable trackpad and enables it (t and y)
   ,((modm .|. shiftMask, xK_t), spawn "xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Device Enabled' 0")
   ,((modm .|. shiftMask, xK_y), spawn "xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Device Enabled' 1")
-
-    --,((modm, xK_a), sendMessage NextLayout >> (dynamicLogString xmobarPP >>= \d->spawn $"xmessage "++d))
-    ]
+    ]++
+    [((m .|. modm, k), windows $ f i)
+         | (i, k) <- zip customWs [xK_1 .. xK_9]
+         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $  mateConfig
                 { modMask = mod4Mask
-                 ,logHook = dynamicLogWithPP $ xmobarPP
+                 ,layoutHook = customLayout
+                 ,logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
-                 , borderWidth = 4
+                        } >> updatePointer Nearest
+
+              , borderWidth = 4
                  , focusedBorderColor = "#7FBC71"
                  , manageHook = manageDocks <+> ownManageHook
                 } `additionalKeysP` myKeys
 
-myKeys = [  (("M4-f"), runOrRaise "firefox" (className =? "Firefox"))
-           ,(("M4-e"), runOrRaise "/home/massyl/softs/scala/eclipse/eclipse" (className =? "Eclipse"))
-           ,(("M4-n"), runOrRaise "emacs" (className =? "Emacs"))
-           --,(("M4-i"), spawn "pidgin")
-           --,(("M4-c"), spawn "google-chrome")
+myKeys = [ (("M4-f") , runOrRaise "firefox" (className =? "Firefox"))
+           ,(("M4-c"),
+             runOrRaise "/home/massyl/softs/eclipse/eclipse.sh" (className =? "Eclipse"))
+           ,(("M4-n"), runOrRaise "/home/massyl/softs/shell/start-emacs.sh" (className =? "Emacs"))
            ,(("M4-z"), kill)
            ,(("M4-p"), spawn "dmenu_run | dmenu -b")
          ]
+
+-- togglevga = do
+--   screencount <- LIS.countScreens
+--   if screencount > 1
+--    then spawn "xrandr --output VGA1 --off"
+--   else spawn "xrandr --output VGA1 --auto --right-of LVDS1"
+
+customLayout = avoidStruts $ tiled ||| Mirror tiled ||| Full
+  where
+    tiled = spacing 5 $ Tall nmaster delta ratio
+    nmaster = 2
+    ratio = 1/3
+    delta = 5/100
