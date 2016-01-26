b5e76429a5ae822c4c60a43be919df760d7b76d3
massyl nait <massil.nait@google.com>
2016-01-26 11:32:34 +0100

    fixing some key mapping for new firefox

--- .keysnail.js
+++ .keysnail.js
@@ -383,11 +383,18 @@ key.setEditKey('M-y', command.yankPop, 'Paste pop (Yank pop)', true);
 
 key.setEditKey('C-M-y', function (ev) {
     if (!command.kill.ring.length) {
-        return;
+           return;
     }
-    let (ct = command.getClipboardText()) (!command.kill.ring.length || ct != command.kill.ring[0]) &&
+    let ct = command.getClipboardText();
+    if (!command.kill.ring.length || ct != command.kill.ring[0]){
         command.pushKillRing(ct);
-    prompt.selector({message: "Paste:", collection: command.kill.ring, callback: function (i) {if (i >= 0) {key.insertText(command.kill.ring[i]);}}});
+     }
+    prompt.selector(
+        { message: "Paste:",
+          collection: command.kill.ring,
+          callback: function (i) { if (i >= 0) {key.insertText(command.kill.ring[i]);}}}
+    );
+
 }, 'Show kill-ring and select text to paste', true);
 
 key.setEditKey('C-w', function (ev) {
